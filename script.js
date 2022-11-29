bbfb5a7da976a73d4239f26d1e4c1f0f36905563
vldrad <97199817+vldrad@users.noreply.github.com>
2022-11-29 22:07:53 +0100

    little fix in typeof() = string

--- script.js
+++ script.js
@@ -1,33 +1,35 @@
 "use strict";
 
 function weightIndex (weight, height) {
-    const greeting = alert('Вітаю! Тут ви можете дізнатись індекс маси вашого тіла. Для цього виміряйте свої дані (вагу та зріст) та введіть їх згідно інструкцій.')
-    const userWeight = prompt('Ваша вага? (в кг)','');
-    const userHeight = prompt('Ваш зріст? (в см)','');
-    weight = userWeight;
-    height = userHeight;
-    let index = weight / ((height / 100) * 2);
-    index = (String(index));
-    if (userWeight == null && userWeight == '' && userHeight == null && userHeight == null) {
-        const indexError = alert('Щось пішло не так , будь-ласка перевірте введені дані та спробуйте ще раз.');
+    for (let i = 1; i >= 1; i++) {
+        const greeting = alert('Вітаю! Тут ви можете дізнатись індекс маси вашого тіла. Для цього виміряйте свої дані (вагу та зріст) та введіть їх згідно інструкцій.')
         const userWeight = prompt('Ваша вага? (в кг)','');
         const userHeight = prompt('Ваш зріст? (в см)','');
-    } else {
-        if (index < 16) {
-            const a = alert(`Ваш ІМТ (Індекс Маси Тіла): ${index.slice(0, 4)}\nУ вас значний дефіцит маси тіла.`);
-        } else if (index == 16 || index < 18.5) {
-            const b = alert(`Ваш ІМТ (Індекс Маси Тіла): ${index.slice(0, 4)}\nУ вас дефіцит маси тіла.`);
-        } else if (index == 18.5 || index < 25) {
-            const d = alert(`Ваш ІМТ (Індекс Маси Тіла): ${index.slice(0, 4)}\nМаса тіла в нормі.`);
-        } else if (index == 25 || index < 30) {
-            const c = alert(`Ваш ІМТ (Індекс Маси Тіла): ${index.slice(0, 4)}\nУ вас є зайва вага.`);
-        } else if (index == 30 || index < 35) {
-            const d = alert(`Ваш ІМТ (Індекс Маси Тіла): ${index.slice(0, 4)}\nУ вас ожиріння першого ступеню.`);
-        } else if (index == 35 || index < 40) {
-            const e = alert(`Ваш ІМТ (Індекс Маси Тіла): ${index.slice(0, 4)}\nУ вас ожиріння другого ступеню.`);
-        } else if (index == 40 || index > 40) {
-            const f = alert(`Ваш ІМТ (Індекс Маси Тіла): ${index.slice(0, 4)}\nУ вас ожиріння третього ступеню.`);
+        weight = userWeight;
+        height = userHeight;
+        let index = weight / ((height / 100) * 2);
+        index = (String(index));
+        if (userWeight == null || userWeight == '' || typeof(userWeight) == 'string' || userHeight == null || userHeight == null || typeof(userHeight) == 'string') {
+            const indexError = alert('Щось пішло не так , будь-ласка перевірте введені дані та спробуйте ще раз.');
+            i--;
+        } else {
+            if (index < 16) {
+                const a = alert(`Ваш ІМТ (Індекс Маси Тіла): ${index.slice(0, 4)}\nУ вас значний дефіцит маси тіла.`);
+            } else if (index == 16 || index < 18.5) {
+                const b = alert(`Ваш ІМТ (Індекс Маси Тіла): ${index.slice(0, 4)}\nУ вас дефіцит маси тіла.`);
+            } else if (index == 18.5 || index < 25) {
+                const d = alert(`Ваш ІМТ (Індекс Маси Тіла): ${index.slice(0, 4)}\nМаса тіла в нормі.`);
+            } else if (index == 25 || index < 30) {
+                const c = alert(`Ваш ІМТ (Індекс Маси Тіла): ${index.slice(0, 4)}\nУ вас є зайва вага.`);
+            } else if (index == 30 || index < 35) {
+                const d = alert(`Ваш ІМТ (Індекс Маси Тіла): ${index.slice(0, 4)}\nУ вас ожиріння першого ступеню.`);
+            } else if (index == 35 || index < 40) {
+                const e = alert(`Ваш ІМТ (Індекс Маси Тіла): ${index.slice(0, 4)}\nУ вас ожиріння другого ступеню.`);
+            } else if (index == 40 || index > 40) {
+                const f = alert(`Ваш ІМТ (Індекс Маси Тіла): ${index.slice(0, 4)}\nУ вас ожиріння третього ступеню.`);
+            }
         }
     }
+    return;
 } 
 weightIndex();
