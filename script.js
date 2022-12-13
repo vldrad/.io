1131c8459f5487f320e7e1b5d74da860c75263b5
vldrad <97199817+vldrad@users.noreply.github.com>
2022-12-13 13:45:21 +0100

    otkat

--- script.js
+++ script.js
@@ -70,6 +70,3 @@ personalMovieDB.rememberMyFilms();
 personalMovieDB.writeYourGenres();
 // personalMovieDB.toggleVisibleMyDB();
 personalMovieDB.showMyDB();
-console.log(`Фильмов просмотрено : ${personalMovieDB.count}\n
-             Последние просмотренные : ${personalMovieDB.movies}\n
-             Любимые жанры : ${personalMovieDB.genres}`);
