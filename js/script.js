017ed6af11b177f441baa099c31e234d834dced6
VladRad <vldrad666@gmail.com>
2023-02-14 18:04:46 +0100

    Added touch support

--- js/script.js
+++ js/script.js
@@ -7,14 +7,16 @@ const ufo = document.querySelector('#ufo'),
 
 
 
-document.addEventListener('keydown', () => {
-    jump();
-    gameOver.style.display = 'none';
-    retry.style.display = 'none';
-    house.style.animationName = 'move';
+document.addEventListener('keydown', (k) => {
+    if(k.code == 'Space') {
+        jump();
+        gameOver.style.display = 'none';
+        retry.style.display = 'none';
+        house.style.animationName = 'move';
+    };
 });
 
-document.addEventListener('ontouchstart', () => {
+document.addEventListener('touchstart', () => {
     jump();
     gameOver.style.display = 'none';
     retry.style.display = 'none';
@@ -34,10 +36,12 @@ let liveCheck = setInterval(() => {
     let ufoTop = parseInt(window.getComputedStyle(ufo).getPropertyValue('top'));
     let houseLeft = parseInt(window.getComputedStyle(house).getPropertyValue('left'));
 
-    if(houseLeft > -110 && houseLeft < 125 && ufoTop > 210) {
-        gameOver.style.display = 'block';
-        retry.style.display = 'block';
-        house.style.animationName = 'none';
+    for(let i = 0; i < 2; i++) {
+        if(houseLeft > -110 && houseLeft < 125 && ufoTop > 210) {
+            gameOver.style.display = 'block';
+            retry.style.display = 'block';
+            house.style.animationName = 'none';
+        }
     }
 }, 10);
 
