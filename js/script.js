6d92a3292f4ee6d1df7c17be3296dd72faae631d
vldrad <97199817+vldrad@users.noreply.github.com>
2024-05-09 01:14:30 +0200

    Files loaded

--- js/script.js
+++ js/script.js
@@ -1,20 +1,73 @@
-'use strict';
+"use strict";
 
+const screen = document.querySelector('#screen'),
+      btn = document.querySelector('button'),
+      waiting = new Audio('sounds/waiting.mp3');
 
-const soundClick = new Audio('../sound/switch-sound.mp3');
+function yesNo() {
+    waiting.pause();
+    waiting.currentTime = 0;
 
-const acceptButton = document.querySelector('.enter_window button'),
-      rightJoyCon = document.querySelector('.right'),
-      disclaimer = document.querySelector('.enter_window'),
-      content = document.querySelector('.content');
+    let num = Math.floor(Math.random() * 1000);
+                
+    if (num % 2 == 0) {
+        screen.innerHTML = 'Unfortunately, YOU ARE NOT GAY!';
+        new Audio('sounds/NO.mp3').play();
+        console.log('User is not gay.');
+    } else {
+        screen.innerHTML = 'Congratulations, YOU ARE GAY!';
+        new Audio('sounds/YES.mp3').play();
+        console.log('User is gay.');
+    }
+}
 
-acceptButton.addEventListener('click', () => {
-    soundClick.play();
-    rightJoyCon.classList.add('joycon_anim');
-    rightJoyCon.style.top = '0';
-    disclaimer.style.display = 'none';
-    content.style.display = 'flex';
-    setTimeout(() => {
-        rightJoyCon.classList.remove('joycon_anim');
-    }, 1000);
-});
+function startCheck() {
+    let finished = false;
+
+    if (!finished) {
+        btn.addEventListener('click', (e) => {
+            waiting.play();
+            e.target.style.display = 'none';
+    
+            screen.innerHTML = 'Please, wait...'
+    
+            setTimeout(() => {
+                screen.innerHTML = 'Asking your friends...';
+            }, 5000);
+    
+            setTimeout(() => {
+                screen.innerHTML = 'Checking your search history...';
+            }, 11000);
+    
+            setTimeout(() => {
+                screen.innerHTML = 'Talking with your ex...';
+            }, 14000);
+    
+            setTimeout(() => {
+                screen.innerHTML = 'So annoying bitch -__- ...';
+            }, 21000);
+    
+            setTimeout(() => {
+                screen.innerHTML = 'Scanning your playlist...';
+            }, 24000);
+            
+            setTimeout(() => {
+                screen.innerHTML = 'Almost done...';
+            }, 28000);
+    
+            setTimeout(() => {
+                yesNo();
+                finished = true;
+                
+                btn.innerHTML = 'AGAIN?'
+                btn.style.display = 'flex';
+            }, 32000);
+        });
+    } else {
+        location.reload();
+    }
+}
+
+startCheck();
+
+console.log();
